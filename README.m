f6a6274761cb25373484cc57edb8fbd5f8c1b83b
Griffin-Patel <glp56@cornell.edu>
2026-02-04 14:59:18 -0500

    first of three git pushes to add all the files from ontario-report

---
+++ README.m
@@ -0,0 +1,7 @@
+# Lake Ontario microbial & Environmental Analysis 
+
+**DEscription** Explore the microbes that live in Lake Ontario as part of a project in BIOMI4300 in the Spring of 2026.
+**Author** Griffin Patel 
+**Date** Feb 4 2026
+
+ 
